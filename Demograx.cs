829133e2e2d93c7abb6e8a5d755ed87debca5074
Talha Ali <40734718@live.napier.ac.uk>
2025-02-17 09:46:03 +0000

    Add files via upload

---
+++ Demograx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DemograxExecutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set the background color and text color
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+
+            Console.Clear();
+
+
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("### Welcome To DemograX ###");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\nThis is an executable file for Demograx.");
+
+
+            Console.ResetColor();
+
+
+            Console.ReadLine();
+
+      
+
+        }
+    }
+}
